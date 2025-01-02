7597ae883c67e9e4cb1d27bdd84ead31c1fafb1d
mlataza <1750947+mlataza@users.noreply.github.com>
2025-01-02 15:10:38 +0800

    update shebang to autoresolve pwsh

--- scripts/clean.ps1
+++ scripts/clean.ps1
@@ -1,4 +1,4 @@
-#! /usr/bin/pwsh
+#! /usr/bin/env pwsh
 
 Remove-Item "./bin/rewst_remote_agent.exe" -ErrorAction SilentlyContinue
 Remove-Item "./bin/rewst_remote_agent" -ErrorAction SilentlyContinue
