a07e4f62f08279d44d4d9a5d3f19a66eacf19fbd
mlataza <1750947+mlataza@users.noreply.github.com>
2025-02-21 21:34:30 +0800

    fix: move paths retrieval to main function

--- scripts/mock.win.ps1
+++ scripts/mock.win.ps1
@@ -1,7 +1,7 @@
-# USED FOR LOCAL DEV ONLY
-$orgId = $env:REWST_ORG_ID
-Copy-Item -Path "./bin/rewst_remote_agent.win.exe" -Destination "C:/Program Files/RewstRemoteAgent/$orgId/rewst_remote_agent_$orgId.win.exe"
-Copy-Item -Path "./bin/rewst_windows_service.win.exe" -Destination "C:/Program Files/RewstRemoteAgent/$orgId/rewst_windows_service_$orgId.win.exe"
+# Used for local developments
+Copy-Item -Path "./bin/rewst_remote_agent.win.exe" -Destination "C:/Program Files/RewstRemoteAgent/$env:REWST_ORG_ID/rewst_remote_agent_$env:REWST_ORG_ID.win.exe"
+Copy-Item -Path "./bin/rewst_windows_service.win.exe" -Destination "C:/Program Files/RewstRemoteAgent/$env:REWST_ORG_ID/rewst_windows_service_$env:REWST_ORG_ID.win.exe"
+Copy-Item -Path "./bin/rewst_service_manager.win.exe" -Destination "C:/Program Files/RewstRemoteAgent/$env:REWST_ORG_ID/rewst_service_manager$env:REWST_ORG_ID.win.exe"
 
-sc.exe delete "RewstRemoteAgent_$orgId"
-sc.exe create "RewstRemoteAgent_$orgId" binPath="C:/Program Files/RewstRemoteAgent/$orgId/rewst_windows_service_$orgId.win.exe"
+sc.exe delete "RewstRemoteAgent_$env:REWST_ORG_ID"
+sc.exe create "RewstRemoteAgent_$env:REWST_ORG_ID" binPath="C:/Program Files/RewstRemoteAgent/$env:REWST_ORG_ID/rewst_windows_service_$env:REWST_ORG_ID.win.exe --org-id $env:REWST_ORG_ID"
