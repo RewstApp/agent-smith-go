c8db8314e0b0f0161518fa69a58d9eb957322eac
mlataza <1750947+mlataza@users.noreply.github.com>
2025-02-21 22:40:20 +0800

    feat: improve service manager

--- scripts/build.win.ps1
+++ scripts/build.win.ps1
@@ -1,5 +1,5 @@
 # Build the executables for windows
-go build -o "./bin/rewst_remote_agent.win.exe" "./cmd/rewst_remote_agent"
-go build -o "./bin/rewst_windows_service.win.exe" "./cmd/rewst_windows_service"
-go build -o "./bin/rewst_agent_config.win.exe" "./cmd/rewst_agent_config"
-go build -o "./bin/rewst_service_manager.win.exe" "./cmd/rewst_service_manager"
+go build -ldflags="-w -s" -o "./bin/rewst_remote_agent.win.exe" "./cmd/rewst_remote_agent"
+go build -ldflags="-w -s" -o "./bin/rewst_windows_service.win.exe" "./cmd/rewst_windows_service"
+go build -ldflags="-w -s" -o "./bin/rewst_agent_config.win.exe" "./cmd/rewst_agent_config"
+go build -ldflags="-w -s" -o "./bin/rewst_service_manager.win.exe" "./cmd/rewst_service_manager"
