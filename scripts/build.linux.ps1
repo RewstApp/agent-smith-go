2a7d17776e4186bb88929f2408d393e181c3d4de
mlataza <1750947+mlataza@users.noreply.github.com>
2025-03-27 22:19:23 +0800

    fix: update linux build script description

--- scripts/build.linux.ps1
+++ scripts/build.linux.ps1
@@ -1,4 +1,4 @@
-# Build the executables for windows
+# Build the executables for linux
 $env:GOARCH = "amd64" # Use 64-bit as default architecture
 $env:GOOS = "linux"
 $versionFlag = "-X github.com/RewstApp/agent-smith-go/internal/version.Version=v$(cz version -p)"
