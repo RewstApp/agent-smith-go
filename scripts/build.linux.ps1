64993a540b43cb3f26da7895851b94d6cda32941
mlataza <1750947+mlataza@users.noreply.github.com>
2025-03-29 17:52:44 +0800

    refactor: remove unused scripts

--- scripts/build.linux.ps1
+++
@@ -1,5 +0,0 @@
-# Build the executables for linux
-$env:GOARCH = "amd64" # Use 64-bit as default architecture
-$env:GOOS = "linux"
-$versionFlag = "-X github.com/RewstApp/agent-smith-go/internal/version.Version=v$(cz version -p)"
-go build -ldflags="-w -s $versionFlag" -o "./dist/rewst_agent_config.linux.bin" "./cmd/agent_smith"
