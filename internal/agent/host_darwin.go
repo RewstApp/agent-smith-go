12b1e4c70d5a2652594c8e343924c8c4dea112a7
mlataza <1750947+mlataza@users.noreply.github.com>
2025-08-06 22:47:19 +0800

    feat: add support for darwin-specific functions

---
+++ internal/agent/host_darwin.go
@@ -0,0 +1,44 @@
+//go:build darwin
+
+package agent
+
+import (
+	"context"
+	"fmt"
+	"net"
+	"strings"
+)
+
+func getAdDomain(context.Context) (*string, error) {
+	return nil, nil
+}
+
+func getIsAdDomainController(context.Context) (bool, error) {
+	return false, nil
+}
+
+func getIsEntraConnectServer() (bool, error) {
+	return false, nil
+}
+
+func getMacAddress() (*string, error) {
+	ifas, err := net.Interfaces()
+	if err != nil {
+		return nil, err
+	}
+
+	for _, ifa := range ifas {
+		a := ifa.HardwareAddr.String()
+		if len(a) > 0 {
+			// Replace : with empty string
+			a = strings.ReplaceAll(a, ":", "")
+			return &a, nil
+		}
+	}
+
+	return nil, fmt.Errorf("%s", "No mac address found")
+}
+
+func getEntraDomain(context.Context) (*string, error) {
+	return nil, nil
+}
