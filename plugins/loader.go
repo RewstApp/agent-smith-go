a8312a1bc9021d9e7c5fd7f4e9b7ae28102dd5d7
mlataza <1750947+mlataza@users.noreply.github.com>
2025-08-28 05:52:48 +0800

    fix: cleanup plugin loader

--- plugins/loader.go
+++ plugins/loader.go
@@ -8,17 +8,12 @@ import (
 	"github.com/hashicorp/go-plugin"
 )
 
-// handshakeConfigs are used to just do a basic handshake between
-// a plugin and host. If the handshake fails, a user friendly error is shown.
-// This prevents users from executing bad plugins or executing a plugin
-// directory. It is a UX feature, not a security feature.
 var handshakeConfig = plugin.HandshakeConfig{
 	ProtocolVersion:  1,
 	MagicCookieKey:   "BASIC_PLUGIN",
 	MagicCookieValue: "hello",
 }
 
-// pluginMap is the map of plugins we can dispense.
 var pluginMap = map[string]plugin.Plugin{
 	"notifier": &shared.NotifierPlugin{},
 }
@@ -52,20 +47,16 @@ func LoadNotifer(path string, logWriter io.Writer) (shared.Notifier, error) {
 		Stderr:          logWriter,
 	})
 
-	// Connect via RPC
 	rpcClient, err := client.Client()
 	if err != nil {
 		return &optionalNotifierWrapper{}, err
 	}
 
-	// Request the plugin
 	raw, err := rpcClient.Dispense("notifier")
 	if err != nil {
 		return &optionalNotifierWrapper{}, err
 	}
 
-	// We should have a Greeter now! This feels like a normal interface
-	// implementation but is in fact over an RPC connection.
 	return &optionalNotifierWrapper{
 		client: client,
 		plugin: raw.(shared.Notifier),
